455f7061d53d6a4f8bd548aa6d33bcfb8a7d2a2c
Nan Cao <nan.cao@kahosl.be>
2012-10-29 16:57:53 +0100

    Add sigleton, add delegate and event for capturing the output
    The IO_Git class is now a implementatie of sigleton.
    IO_Git now can be monitored for the output.
    Test class is created for testing of these new changes.

---
+++ NasGit/IO/IO_Git.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace NasGit.IO {
+    /// <summary>
+    /// This class is a sigleton class used to run git commando's in windows.
+    /// In order to prevent excess instance, and for easier to catch the output.
+    /// To get a instance of this class, use "IO_Git.Git;"
+    /// </summary>
+    public  class IO_Git {
+        /// <summary>
+        /// implementation of sigleton
+        /// </summary>
+        private static IO_Git git;
+        public static IO_Git Git {
+            get {
+                if (git == null) {
+                    git = new IO_Git();
+                }
+                return git;
+            }
+        }
+        /// <summary>
+        /// In this instance contains the "git.exe" process, some cofigurations, and the arguments.
+        /// </summary>
+        private ProcessStartInfo start;
+        /// <summary>
+        /// delegate as a uniform of eventhandeler of event OutputDataRecevied
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public delegate void OutputDelegate(object sender, DataReceivedEventArgs e);
+        /// <summary>
+        /// This event will be trigged when new output is added in the output list. So the classed in logic layer can catch and show it on forms. 
+        /// </summary>
+        public event OutputDelegate OutputDataReceived;
+        
+        /// <summary>
+        /// This list of string contains every output form the commando's.
+        /// </summary>
+        private List<string> output;
+        public List<string> Output {
+            get { return output; }
+        }
+
+        /// <summary>
+        /// This private constructor will make sure this class is a sigleton class.
+        /// </summary>
+        private IO_Git() { 
+            start = new ProcessStartInfo("git.exe");
+            output = new List<string>();
+        }
+        /// <summary>
+        /// This static methode will create a new repository, which is a copy of remote resource. 
+        /// </summary>
+        /// <param name="address">The address of the remote resource.</param>
+        /// <param name="dir">The folder witch the repository will be created in.</param>
+        /// <returns>The StreamReader instace contains the output.</returns>
+        public void CloneFromRemote(string address,string dir="") {
+            //set the command for cloning from remote
+            start.Arguments = "clone "+address+" "+dir; 
+            //hide the cmd window
+            start.CreateNoWindow = true;
+            //redirect the output
+            start.RedirectStandardOutput = true;
+            //redirect the input
+            start.RedirectStandardInput = true;
+            //in order to redirect, this has to be set false.
+            start.UseShellExecute = false;
+            //start the process
+            Process process = new Process();//Process.Start(start);
+            //every time the process send some output, the handler will be trigged.
+            process.OutputDataReceived += new DataReceivedEventHandler(ReceiveOutput);
+            //process.o
+        }
+
+        /// <summary>
+        /// EventHandeler for receving the output.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ReceiveOutput(object sender, DataReceivedEventArgs e) {
+            output.Add(e.Data);
+            OutputDataReceived(output, e);
+        }
+    }
+}
---
+++ NasGit/IO/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NasGit.IO {
+    class Test {
+        public Test(){
+            IO_Git.Git.OutputDataReceived += new IO_Git.OutputDelegate(OutputReceivedHandeler);
+        }
+
+        private void OutputReceivedHandeler(object sender, System.Diagnostics.DataReceivedEventArgs e) {
+            Console.Write(e.Data);
+        }
+    }
+}
