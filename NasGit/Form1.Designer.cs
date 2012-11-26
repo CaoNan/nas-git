c811d59d0650d7fdeae08664befdc0793451cd77
Sander Weyens <sander.weyens@kahosl.be>
2012-11-26 21:13:13 +0100

    Added list of commits, init repo, ...
    + list of commits
    + list of changed files
    + start of simple diff view
    + add/init repository

--- NasGit/Form1.Designer.cs
+++ NasGit/Form1.Designer.cs
@@ -33,13 +33,21 @@
             this.tsbSettings = new System.Windows.Forms.ToolStripButton();
             this.fswCheckFilechanges = new System.IO.FileSystemWatcher();
             this.fbdAddRepository = new System.Windows.Forms.FolderBrowserDialog();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.lvCommits = new System.Windows.Forms.ListView();
+            this.Revision = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Message = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Author = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Age = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.lblChangeset = new System.Windows.Forms.Label();
             this.lblBranch = new System.Windows.Forms.Label();
             this.lblUser = new System.Windows.Forms.Label();
+            this.lvModifiedFiles = new System.Windows.Forms.ListView();
+            this.fileName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.hash = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.rtbDiff = new System.Windows.Forms.RichTextBox();
             this.tcRepositories.SuspendLayout();
             this.toolStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.fswCheckFilechanges)).BeginInit();
@@ -47,8 +55,7 @@
             // 
             // tcRepositories
             // 
-            this.tcRepositories.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.tcRepositories.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tcRepositories.Controls.Add(this.tpRepository);
             this.tcRepositories.Controls.Add(this.tabPage1);
@@ -56,7 +63,7 @@
             this.tcRepositories.Location = new System.Drawing.Point(12, 54);
             this.tcRepositories.Name = "tcRepositories";
             this.tcRepositories.SelectedIndex = 0;
-            this.tcRepositories.Size = new System.Drawing.Size(769, 27);
+            this.tcRepositories.Size = new System.Drawing.Size(769, 20);
             this.tcRepositories.TabIndex = 0;
             // 
             // tpRepository
@@ -64,7 +71,7 @@
             this.tpRepository.Location = new System.Drawing.Point(4, 22);
             this.tpRepository.Name = "tpRepository";
             this.tpRepository.Padding = new System.Windows.Forms.Padding(3);
-            this.tpRepository.Size = new System.Drawing.Size(761, 1);
+            this.tpRepository.Size = new System.Drawing.Size(761, 0);
             this.tpRepository.TabIndex = 1;
             this.tpRepository.Text = "Repository";
             // 
@@ -73,7 +80,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(761, 1);
+            this.tabPage1.Size = new System.Drawing.Size(761, 0);
             this.tabPage1.TabIndex = 2;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -117,6 +124,7 @@
             this.tsbCloneRepository.Name = "tsbCloneRepository";
             this.tsbCloneRepository.Size = new System.Drawing.Size(38, 38);
             this.tsbCloneRepository.ToolTipText = "Clone repository";
+            this.tsbCloneRepository.Click += new System.EventHandler(this.tsbCloneRepository_Click);
             // 
             // tsbSettings
             // 
@@ -139,18 +147,47 @@
             // 
             this.fbdAddRepository.RootFolder = System.Environment.SpecialFolder.MyDocuments;
             // 
-            // listView1
+            // lvCommits
             // 
-            this.listView1.Location = new System.Drawing.Point(12, 154);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(300, 333);
-            this.listView1.TabIndex = 4;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.lvCommits.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Revision,
+            this.Message,
+            this.Author,
+            this.Age});
+            this.lvCommits.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.lvCommits.Location = new System.Drawing.Point(12, 82);
+            this.lvCommits.MultiSelect = false;
+            this.lvCommits.Name = "lvCommits";
+            this.lvCommits.Size = new System.Drawing.Size(765, 138);
+            this.lvCommits.TabIndex = 4;
+            this.lvCommits.UseCompatibleStateImageBehavior = false;
+            this.lvCommits.View = System.Windows.Forms.View.Details;
+            this.lvCommits.SelectedIndexChanged += new System.EventHandler(this.lvCommits_SelectedIndexChanged);
+            // 
+            // Revision
+            // 
+            this.Revision.Text = "Revision";
+            this.Revision.Width = 80;
+            // 
+            // Message
+            // 
+            this.Message.Text = "Message";
+            this.Message.Width = 440;
+            // 
+            // Author
+            // 
+            this.Author.Text = "Author";
+            this.Author.Width = 100;
+            // 
+            // Age
+            // 
+            this.Age.Text = "Age";
+            this.Age.Width = 80;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 88);
+            this.label1.Location = new System.Drawing.Point(331, 226);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(61, 13);
             this.label1.TabIndex = 5;
@@ -159,7 +196,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 105);
+            this.label2.Location = new System.Drawing.Point(331, 243);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(44, 13);
             this.label2.TabIndex = 6;
@@ -168,7 +205,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 122);
+            this.label3.Location = new System.Drawing.Point(331, 260);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(32, 13);
             this.label3.TabIndex = 7;
@@ -177,7 +214,7 @@
             // lblChangeset
             // 
             this.lblChangeset.AutoSize = true;
-            this.lblChangeset.Location = new System.Drawing.Point(79, 88);
+            this.lblChangeset.Location = new System.Drawing.Point(398, 226);
             this.lblChangeset.Name = "lblChangeset";
             this.lblChangeset.Size = new System.Drawing.Size(320, 13);
             this.lblChangeset.TabIndex = 8;
@@ -186,7 +223,7 @@
             // lblBranch
             // 
             this.lblBranch.AutoSize = true;
-            this.lblBranch.Location = new System.Drawing.Point(81, 105);
+            this.lblBranch.Location = new System.Drawing.Point(400, 243);
             this.lblBranch.Name = "lblBranch";
             this.lblBranch.Size = new System.Drawing.Size(39, 13);
             this.lblBranch.TabIndex = 9;
@@ -195,24 +232,58 @@
             // lblUser
             // 
             this.lblUser.AutoSize = true;
-            this.lblUser.Location = new System.Drawing.Point(81, 122);
+            this.lblUser.Location = new System.Drawing.Point(400, 260);
             this.lblUser.Name = "lblUser";
             this.lblUser.Size = new System.Drawing.Size(42, 13);
             this.lblUser.TabIndex = 10;
             this.lblUser.Text = "SaWey";
             // 
+            // lvModifiedFiles
+            // 
+            this.lvModifiedFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.fileName,
+            this.hash});
+            this.lvModifiedFiles.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.lvModifiedFiles.Location = new System.Drawing.Point(12, 226);
+            this.lvModifiedFiles.Name = "lvModifiedFiles";
+            this.lvModifiedFiles.Size = new System.Drawing.Size(300, 364);
+            this.lvModifiedFiles.TabIndex = 11;
+            this.lvModifiedFiles.UseCompatibleStateImageBehavior = false;
+            this.lvModifiedFiles.View = System.Windows.Forms.View.Details;
+            this.lvModifiedFiles.SelectedIndexChanged += new System.EventHandler(this.lvModifiedFiles_SelectedIndexChanged);
+            // 
+            // fileName
+            // 
+            this.fileName.Text = "name";
+            this.fileName.Width = 291;
+            // 
+            // hash
+            // 
+            this.hash.Text = "hash";
+            this.hash.Width = 1;
+            // 
+            // rtbDiff
+            // 
+            this.rtbDiff.Location = new System.Drawing.Point(334, 276);
+            this.rtbDiff.Name = "rtbDiff";
+            this.rtbDiff.Size = new System.Drawing.Size(443, 314);
+            this.rtbDiff.TabIndex = 12;
+            this.rtbDiff.Text = "";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(793, 499);
+            this.ClientSize = new System.Drawing.Size(793, 602);
+            this.Controls.Add(this.rtbDiff);
+            this.Controls.Add(this.lvModifiedFiles);
             this.Controls.Add(this.lblUser);
             this.Controls.Add(this.lblBranch);
             this.Controls.Add(this.lblChangeset);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.lvCommits);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.tcRepositories);
             this.Name = "Form1";
@@ -242,9 +313,17 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.Label lblUser;
         private System.Windows.Forms.Label lblBranch;
+        private System.Windows.Forms.RichTextBox rtbDiff;
+        private System.Windows.Forms.ListView lvModifiedFiles;
+        private System.Windows.Forms.ColumnHeader Revision;
+        private System.Windows.Forms.ColumnHeader Message;
+        private System.Windows.Forms.ColumnHeader Author;
+        private System.Windows.Forms.ColumnHeader Age;
+        private System.Windows.Forms.ListView lvCommits;
+        private System.Windows.Forms.ColumnHeader fileName;
+        private System.Windows.Forms.ColumnHeader hash;
     }
 }
 
--- NasGit/Form1.cs
+++ NasGit/Form1.cs
@@ -1,47 +1,194 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-
-using NasGit.IO;
-using System.IO;
+﻿using LibGit2Sharp;
+using System;
 using System.Diagnostics;
-using NasGit.Logic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
 
 namespace NasGit {
     public partial class Form1 : Form {
-		private GitEngine engine;
+        string mRepo = "C:/Users/Sander/Documents/GitHub/nas-git/";
+        Repository currentRepo;
 		
+        /// <summary>
+        /// Initialise form
+        /// </summary>
         public Form1() {
             InitializeComponent();
-			engine = new GitEngine();
-            checkStatus();
+
+            //Load configuration and repositories
+            currentRepo = new Repository(mRepo);
+            //buildCommitLog();
+            
         }
 
-        private void checkStatus(){
-            string cmd = "status";
-            //IO_Git.Git.OutputDataReceived += new IO_Git.OutputDelegate(OutputReceivedHandeler);
-            //IO_Git.Git.RunGitCommand(cmd);
+
+
+        /// <summary>
+        /// Change the screen to a new commit
+        /// </summary>
+        /// <param name="sha">sha hash of requested commit</param>
+        private void changeCommit(string sha) {
+            currentRepo = new Repository(mRepo);
+
+            //Commit currentCommit = (Commit)currentRepo.Commits.Where(Commit => Commit.Id.Sha == sha).First();
+            Commit currentCommit = (Commit)currentRepo.Lookup(new ObjectId(sha), GitObjectType.Commit);
+
+            //update file list
+            fillFileList(currentCommit.Tree);
+            
+            //update labels
+            lblUser.Text = currentCommit.Committer.Name;
+            lblChangeset.Text = "(" + currentCommit.Id.ToString().Substring(0,6) + ") " + currentCommit.MessageShort;
+            //lblBranch.Text = currentCommit.
+
         }
 
-        private void tsbAddRepository_Click(object sender, EventArgs e)
-        {
-            if (fbdAddRepository.ShowDialog() == System.Windows.Forms.DialogResult.OK) { 
-                //Check if we can create a new repo in this directory
-                if (Directory.EnumerateFileSystemEntries(fbdAddRepository.SelectedPath).Any()) { 
-                    //dir not empty, ask for permission
-                    if (MessageBox.Show("This directory is not empty, are you sure you want to continue?", "Attention", MessageBoxButtons.YesNoCancel) == System.Windows.Forms.DialogResult.OK)
-                    {
-                        //The response is current shown in the output window below.
-                		engine.AddNewRepository(fbdAddRepository.SelectedPath);
-
-                    }
+        /// <summary>
+        /// Fill the list of changed files
+        /// </summary>
+        /// <param name="currentTree">Tree being looked at</param>
+        private void fillFileList(Tree currentTree) {
+            lvModifiedFiles.Items.Clear();
+            addTree(currentTree);
+        }
+        
+        /// <summary>
+        /// Recursive function to loop through commit trees
+        /// </summary>
+        /// <param name="t">current tree</param>
+        /// <param name="prefix">current directory</param>
+        private void addTree(Tree t, string prefix = "") {
+            foreach (TreeEntry te in t)
+            {
+                Debug.WriteLine(te.Target.GetType());
+                if (te.Target.GetType().ToString() == "LibGit2Sharp.Tree")
+                {
+                    Tree b = (Tree)te.Target;
+                    addTree(b, prefix + "/" + te.Name);
+                }
+                else {
+                    ListViewItem itm = new ListViewItem(new string[2]{prefix + "/" + te.Name, te.Target.Sha});
+                    lvModifiedFiles.Items.Add(itm);
                 }
             }
         }
+
+
+        /// <summary>
+        /// Build a log of commits for current repository
+        /// </summary>
+        private void buildCommitLog(){
+            using (currentRepo)
+            {
+                foreach (Commit c in currentRepo.Commits)
+                {
+                    ListViewItem itm = new ListViewItem(new string[4]{c.Id.ToString(), c.MessageShort, c.Author.Name, c.Author.When.ToString()});
+                    lvCommits.Items.Add(itm);
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Try to add a new repository, will discover if directory already is version controlled.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tsbAddRepository_Click(object sender, EventArgs e)
+        {
+            if (fbdAddRepository.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                bool continueCreation = false;
+
+                //Check if we can create a new repo in this directory
+                if (Directory.EnumerateFileSystemEntries(fbdAddRepository.SelectedPath).Any())
+                {
+                    //dir not empty, ask for permission
+                    if (MessageBox.Show("This directory is not empty, are you sure you want to continue?", "Attention", MessageBoxButtons.YesNoCancel) == System.Windows.Forms.DialogResult.Yes)
+                    {
+                        continueCreation = true;
+                    }
+                }
+                else {
+                    continueCreation = true;
+                }
+
+                if (continueCreation) {
+                    if (Repository.Discover(fbdAddRepository.SelectedPath) != null)
+                    {
+                        //repository available here, import it
+                        
+                    }
+                    else { 
+                        //create new repository
+                        try
+                        {
+                            currentRepo = Repository.Init(fbdAddRepository.SelectedPath);
+                            MessageBox.Show("Repository aangemaakt");
+                        }
+                        catch (RepositoryNotFoundException rne)
+                        {
+                            MessageBox.Show(rne.Message);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                        
+                    }
+                    
+                }
+            }
+        }
+
+        /// <summary>
+        /// Clone a repository, localy or remote
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tsbCloneRepository_Click(object sender, EventArgs e)
+        {
+            
+            //Repository.Clone();
+        }
+
+        /// <summary>
+        /// Show the changes made in this commit
+        /// </summary>
+        /// <param name="sha">sha hash of the file</param>
+        private void showDiff(string sha) {
+            //Working dir diff NOT GOOD
+            //TreeChanges tc = currentRepo.Diff.Compare();
+            //WE NEED THE DIFF OF SELECTED COMMIT
+            //TreeEntryChanges tec = tc.Modified.Where(TreeEntryChanges => TreeEntryChanges.Oid.Sha == sha).First();
+
+
+        }
+
+        /// <summary>
+        /// When new commit is selected, update filelist and info labels
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void lvCommits_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lvCommits.SelectedItems.Count > 0) { 
+                changeCommit(lvCommits.SelectedItems[0].Text);
+            }
+            
+        }
+
+        /// <summary>
+        /// When a file in this commit is selected, show the differences with previous version
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void lvModifiedFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lvModifiedFiles.SelectedItems.Count > 0)
+            {
+                showDiff(lvModifiedFiles.SelectedItems[0].SubItems[1].Text);
+            }
+        }
+
     }
 }
