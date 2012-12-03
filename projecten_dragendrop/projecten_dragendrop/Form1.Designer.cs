170e5fc1d398ff5c7b7b449831c1c1f2f4fbc981
Andries Verlinden <andriesverlinden@gmail.com>
2012-12-03 09:42:44 +0100

    edit dropvenster

--- projecten_dragendrop/projecten_dragendrop/Form1.Designer.cs
+++ projecten_dragendrop/projecten_dragendrop/Form1.Designer.cs
@@ -29,9 +29,23 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Node0");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Node6");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Node7");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Node1", new System.Windows.Forms.TreeNode[] {
+            treeNode2,
+            treeNode3});
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Node2");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Node3");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Node4");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Node5");
             this.lblText = new System.Windows.Forms.Label();
             this.nyIcon = new System.Windows.Forms.NotifyIcon(this.components);
-            this.cmbRepository = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblText
@@ -48,16 +62,71 @@
             this.nyIcon.Text = "notifyIcon1";
             this.nyIcon.Visible = true;
             // 
-            // cmbRepository
+            // button1
             // 
-            this.cmbRepository.FormattingEnabled = true;
-            this.cmbRepository.Items.AddRange(new object[] {
-            "rep1",
-            "rep2"});
-            this.cmbRepository.Location = new System.Drawing.Point(77, 12);
-            this.cmbRepository.Name = "cmbRepository";
-            this.cmbRepository.Size = new System.Drawing.Size(121, 21);
-            this.cmbRepository.TabIndex = 1;
+            this.button1.Location = new System.Drawing.Point(77, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "lokale commit";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Node0";
+            treeNode1.Text = "Node0";
+            treeNode2.Name = "Node6";
+            treeNode2.Text = "Node6";
+            treeNode3.Name = "Node7";
+            treeNode3.Text = "Node7";
+            treeNode4.Name = "Node1";
+            treeNode4.Text = "Node1";
+            treeNode5.Name = "Node2";
+            treeNode5.Text = "Node2";
+            treeNode6.Name = "Node3";
+            treeNode6.Text = "Node3";
+            treeNode7.Name = "Node4";
+            treeNode7.Text = "Node4";
+            treeNode8.Name = "Node5";
+            treeNode8.Text = "Node5";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode4,
+            treeNode5,
+            treeNode6,
+            treeNode7,
+            treeNode8});
+            this.treeView1.Size = new System.Drawing.Size(263, 97);
+            this.treeView1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "lijst sorteren op laatst gewijzgd?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "vergroten bij aanraken";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(77, 217);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "open app";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // frmDrop
             // 
@@ -65,7 +134,11 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.cmbRepository);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lblText);
             this.Name = "frmDrop";
             this.Text = "Dropvenster";
@@ -80,7 +153,11 @@
 
         private System.Windows.Forms.Label lblText;
         private System.Windows.Forms.NotifyIcon nyIcon;
-        private System.Windows.Forms.ComboBox cmbRepository;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
     }
 }
 
