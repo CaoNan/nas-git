71945ea2fb57efcdc67b612cdfa414be1dc08ba5
Andries Verlinden <andriesverlinden@gmail.com>
2012-12-16 12:42:41 +0100

    edit dropvenster
    enkel nog commit-commando en go to app toevoegen

--- projecten_dragendrop/projecten_dragendrop/Form1.Designer.cs
+++ projecten_dragendrop/projecten_dragendrop/Form1.Designer.cs
@@ -29,121 +29,141 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Node0");
-            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Node6");
-            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Node7");
-            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Node1", new System.Windows.Forms.TreeNode[] {
-            treeNode2,
-            treeNode3});
-            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Node2");
-            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Node3");
-            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Node4");
-            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Node5");
-            this.lblText = new System.Windows.Forms.Label();
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("master");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("...");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("repo1", new System.Windows.Forms.TreeNode[] {
+            treeNode6,
+            treeNode7});
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("master");
+            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("repo2", new System.Windows.Forms.TreeNode[] {
+            treeNode9});
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDrop));
             this.nyIcon = new System.Windows.Forms.NotifyIcon(this.components);
-            this.button1 = new System.Windows.Forms.Button();
-            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.btnLocalCommit = new System.Windows.Forms.Button();
+            this.trvRepos = new System.Windows.Forms.TreeView();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblDropArea = new System.Windows.Forms.Label();
+            this.pnlDropArea = new System.Windows.Forms.Panel();
+            this.pbcApp = new System.Windows.Forms.PictureBox();
+            this.pnlDropArea.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcApp)).BeginInit();
             this.SuspendLayout();
-            // 
-            // lblText
-            // 
-            this.lblText.AutoSize = true;
-            this.lblText.Location = new System.Drawing.Point(27, 114);
-            this.lblText.Name = "lblText";
-            this.lblText.Size = new System.Drawing.Size(228, 13);
-            this.lblText.TabIndex = 0;
-            this.lblText.Text = "Sleep een bestand of een map naar dit venster";
             // 
             // nyIcon
             // 
             this.nyIcon.Text = "notifyIcon1";
             this.nyIcon.Visible = true;
             // 
-            // button1
+            // btnLocalCommit
             // 
-            this.button1.Location = new System.Drawing.Point(77, 187);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(115, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "lokale commit";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnLocalCommit.Enabled = false;
+            this.btnLocalCommit.Location = new System.Drawing.Point(146, 164);
+            this.btnLocalCommit.Name = "btnLocalCommit";
+            this.btnLocalCommit.Size = new System.Drawing.Size(115, 23);
+            this.btnLocalCommit.TabIndex = 2;
+            this.btnLocalCommit.Text = "lokale commit";
+            this.btnLocalCommit.UseVisualStyleBackColor = true;
+            this.btnLocalCommit.Visible = false;
+            this.btnLocalCommit.Click += new System.EventHandler(this.btnLocalCommit_Click);
             // 
-            // treeView1
+            // trvRepos
             // 
-            this.treeView1.Location = new System.Drawing.Point(12, 12);
-            this.treeView1.Name = "treeView1";
-            treeNode1.Name = "Node0";
-            treeNode1.Text = "Node0";
-            treeNode2.Name = "Node6";
-            treeNode2.Text = "Node6";
-            treeNode3.Name = "Node7";
-            treeNode3.Text = "Node7";
-            treeNode4.Name = "Node1";
-            treeNode4.Text = "Node1";
-            treeNode5.Name = "Node2";
-            treeNode5.Text = "Node2";
-            treeNode6.Name = "Node3";
-            treeNode6.Text = "Node3";
-            treeNode7.Name = "Node4";
-            treeNode7.Text = "Node4";
-            treeNode8.Name = "Node5";
-            treeNode8.Text = "Node5";
-            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode1,
-            treeNode4,
-            treeNode5,
-            treeNode6,
-            treeNode7,
-            treeNode8});
-            this.treeView1.Size = new System.Drawing.Size(263, 97);
-            this.treeView1.TabIndex = 3;
+            this.trvRepos.AllowDrop = true;
+            this.trvRepos.Enabled = false;
+            this.trvRepos.Location = new System.Drawing.Point(129, 77);
+            this.trvRepos.Name = "trvRepos";
+            treeNode6.Name = "Node5";
+            treeNode6.Text = "master";
+            treeNode7.Name = "Node6";
+            treeNode7.Text = "...";
+            treeNode8.Name = "Node0";
+            treeNode8.Text = "repo1";
+            treeNode9.Name = "Node7";
+            treeNode9.Text = "master";
+            treeNode10.Name = "Node1";
+            treeNode10.Text = "repo2";
+            this.trvRepos.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode8,
+            treeNode10});
+            this.trvRepos.Size = new System.Drawing.Size(152, 70);
+            this.trvRepos.TabIndex = 3;
+            this.trvRepos.Visible = false;
+            this.trvRepos.MouseClick += new System.Windows.Forms.MouseEventHandler(this.trvRepos_MouseClick);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 143);
+            this.label1.Location = new System.Drawing.Point(55, 207);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(155, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "lijst sorteren op laatst gewijzgd?";
+            this.label1.Visible = false;
             // 
-            // label2
+            // label3
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 160);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(113, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "vergroten bij aanraken";
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(297, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "bij elke repo leeg veld met new branch om snel toe te voegen";
+            this.label3.Visible = false;
             // 
-            // button2
+            // lblDropArea
             // 
-            this.button2.Location = new System.Drawing.Point(77, 217);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "open app";
-            this.button2.UseVisualStyleBackColor = true;
+            this.lblDropArea.AutoSize = true;
+            this.lblDropArea.Location = new System.Drawing.Point(3, 6);
+            this.lblDropArea.Name = "lblDropArea";
+            this.lblDropArea.Size = new System.Drawing.Size(220, 13);
+            this.lblDropArea.TabIndex = 0;
+            this.lblDropArea.Text = "Sleep een bestand of een map naar dit kader";
+            // 
+            // pnlDropArea
+            // 
+            this.pnlDropArea.AllowDrop = true;
+            this.pnlDropArea.AutoScroll = true;
+            this.pnlDropArea.AutoSize = true;
+            this.pnlDropArea.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlDropArea.Controls.Add(this.lblDropArea);
+            this.pnlDropArea.Location = new System.Drawing.Point(78, 12);
+            this.pnlDropArea.Name = "pnlDropArea";
+            this.pnlDropArea.Size = new System.Drawing.Size(286, 47);
+            this.pnlDropArea.TabIndex = 8;
+            this.pnlDropArea.DragDrop += new System.Windows.Forms.DragEventHandler(this.pnlDropArea_DragDrop);
+            this.pnlDropArea.DragEnter += new System.Windows.Forms.DragEventHandler(this.pnlDropArea_DragEnter);
+            this.pnlDropArea.DragOver += new System.Windows.Forms.DragEventHandler(this.pnlDropArea_DragOver);
+            // 
+            // pbcApp
+            // 
+            this.pbcApp.Image = ((System.Drawing.Image)(resources.GetObject("pbcApp.Image")));
+            this.pbcApp.Location = new System.Drawing.Point(12, 8);
+            this.pbcApp.Name = "pbcApp";
+            this.pbcApp.Size = new System.Drawing.Size(53, 51);
+            this.pbcApp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbcApp.TabIndex = 9;
+            this.pbcApp.TabStop = false;
+            this.pbcApp.Click += new System.EventHandler(this.pbcApp_Click);
             // 
             // frmDrop
             // 
-            this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.label2);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(380, 69);
+            this.Controls.Add(this.pbcApp);
+            this.Controls.Add(this.trvRepos);
+            this.Controls.Add(this.pnlDropArea);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.treeView1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.btnLocalCommit);
             this.Name = "frmDrop";
-            this.Text = "Dropvenster";
-            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.frmDrop_DragDrop);
-            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.frmDrop_DragEnter);
+            this.Text = "nas-git - dropvenster";
+            this.Load += new System.EventHandler(this.frmDrop_Load);
+            this.pnlDropArea.ResumeLayout(false);
+            this.pnlDropArea.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcApp)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -151,13 +171,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lblText;
         private System.Windows.Forms.NotifyIcon nyIcon;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button btnLocalCommit;
+        private System.Windows.Forms.TreeView trvRepos;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblDropArea;
+        private System.Windows.Forms.Panel pnlDropArea;
+        private System.Windows.Forms.PictureBox pbcApp;
     }
 }
 
--- projecten_dragendrop/projecten_dragendrop/Form1.cs
+++ projecten_dragendrop/projecten_dragendrop/Form1.cs
@@ -12,32 +12,126 @@ namespace projecten_dragendrop
 {
     public partial class frmDrop : Form
     {
+
+        private TreeNode lastDragDestination = null;
+        private DateTime lastDragDestinationTime;
+        private bool firstTime = true;
+        private string[] files;
+
         public frmDrop()
         {
             InitializeComponent();
-            this.AllowDrop = true;
-            this.DragEnter += new DragEventHandler(frmDrop_DragEnter);
-            this.DragDrop += new DragEventHandler(frmDrop_DragDrop);
+            this.TopMost = true;
+            btnLocalCommit.Enabled = false;
         }
 
-         void frmDrop_DragEnter(object sender, DragEventArgs e) {
+         void pnlDropArea_DragEnter(object sender, DragEventArgs e)
+        {
+            // option false for exact path
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false))
+            {
+                e.Effect = DragDropEffects.All;
+            }
 
-             // option false for exact path
-             if (e.Data.GetDataPresent(DataFormats.FileDrop, false)) {
-                 e.Effect = DragDropEffects.All;
+        }
+
+         void pnlDropArea_DragDrop(object sender, DragEventArgs e) {
+
+             // clear text first time
+             if (this.firstTime == true) {
+                 lblDropArea.Text = "";
+                 this.firstTime = false;
              }
-                
-        }
 
-         void frmDrop_DragDrop(object sender, DragEventArgs e) {
-             
-             string newLocation = "/files";
              string[] files = (string[]) e.Data.GetData(DataFormats.FileDrop);
+             this.files = files;
+
+             // show in GUI
              foreach (string file in files) {
-                 Console.WriteLine(file);
-                 //File.Move(file, newLocation);
+                 lblDropArea.Text += file + '\n'; 
              }
+
+             // treeview repos active
+             trvRepos.Visible = true;
+             trvRepos.Enabled = true;
             
         }
+
+         void pnlDropArea_DragOver(object sender, DragEventArgs e) {
+
+             //this.trvRepos.ExpandAll();
+             Icon dragDropObject = null;
+             TreeNode dragDropNode = null;
+
+             //always disallow by default
+             //e.Effect = DragDropEffects.None;
+
+             //make sure we have data to transfer
+             if (e.Data.GetDataPresent(typeof(TreeNode)))
+             {
+                 dragDropNode = (TreeNode)e.Data.GetData(typeof(TreeNode));
+                 dragDropObject = (Icon)dragDropNode.Tag;
+             }
+             else if (e.Data.GetDataPresent(typeof(ListViewItem)))
+             {
+                 ListViewItem temp = (ListViewItem)e.Data.GetData(typeof(ListViewItem));
+                 dragDropObject = (Icon)temp.Tag;
+             }
+
+             if (dragDropObject != null)
+             {
+                 TreeNode destinationNode = null;
+                 //get current location
+                 Point pt = new Point(e.X, e.Y);
+                 pt = trvRepos.PointToClient(pt);
+                 destinationNode = trvRepos.GetNodeAt(pt);
+                 if (destinationNode == null)
+                 {
+                     return;
+                 }
+
+                 //if we are on a new object, reset our timer
+                 //otherwise check to see if enough time has passed and expand the destination node
+                 if (destinationNode != lastDragDestination)
+                 {
+                     lastDragDestination = destinationNode;
+                     lastDragDestinationTime = DateTime.Now;
+                 }
+                 else
+                 {
+                     TimeSpan hoverTime = DateTime.Now.Subtract(lastDragDestinationTime);
+                     if (hoverTime.TotalSeconds > 2)
+                     {
+                         destinationNode.Expand();
+                     }
+                 }
+             }
+         }
+
+         private void frmDrop_Load(object sender, EventArgs e)
+         {
+             // ophalen lijst repo's en branches
+         }
+
+         
+
+         private void btnLocalCommit_Click(object sender, EventArgs e)
+         {
+             Console.WriteLine("commit");
+             Console.WriteLine(trvRepos.SelectedNode.FullPath);
+         }
+
+         private void trvRepos_MouseClick(object sender, MouseEventArgs e)
+         {
+             btnLocalCommit.Visible = true;
+             btnLocalCommit.Enabled = true;
+         }
+
+         private void pbcApp_Click(object sender, EventArgs e)
+         {
+             // go to app
+             Console.WriteLine("go to app");
+         }
+
     }
 }
